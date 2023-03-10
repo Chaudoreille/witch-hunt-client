9f943024890b9269576c032e8d8fc668d02b5a46
Delfina Marchetti <delfinamarchetti@hotmail.it>
2023-03-10 19:26:06 +0100

    fix login page, finish signup page(1)

--- src/components/SignupForm/SignupForm.jsx
+++ src/components/SignupForm/SignupForm.jsx
@@ -1,50 +1,49 @@
 import React from "react";
 import "./SignupForm.css";
+import Input from "../Input/Input";
+import Button from "../Button/Button"
+import { Link } from "react-router-dom";
 
 function SignupForm({ handleChange, handleSubmit, user, errors }) {
   return (
-    <form className="SignupForm" onSubmit={handleSubmit}>
-      <div className={errors.includes("username") ? "input-error" : ""}>
-        <label htmlFor="username">Username:</label>
-        <input
-          id="username"
-          type="text"
-          name="username"
-          onChange={handleChange}
-          value={user.username}
-        />
+    <form className="SignupForm Form" onSubmit={handleSubmit}>
+
+      <Input type="username"
+        name="username"
+        action={handleChange}
+        className={errors.includes("username") ? "input-error" : ""}
+        label="Username"
+        value={user.username}
+        placeholder="A funny username"
+      />
+      <Input type="email"
+        name="email"
+        action={handleChange}
+        className={errors.includes("email") ? "input-error" : ""}
+        label="Email"
+        value={user.email}
+        placeholder="A real email"
+      />
+      <Input type="password"
+        name="password"
+        action={handleChange}
+        className={errors.includes("password") ? "input-error" : ""}
+        label="Password"
+        value={user.password}
+        placeholder="A strong password"
+      />
+      <Input type="password"
+        name="confirmation"
+        action={handleChange}
+        className={errors.includes("confirmation") ? "input-error" : ""}
+        label="Repeat password"
+        value={user.confirmation}
+        placeholder="Your password, again"
+      />
+      <Button type="submit" variant={"secondary"} >Next</Button>
+      <div className="form-small-link">
+        <Link to="/login">You already have an account ?</Link>
       </div>
-      <div className={errors.includes("email") ? "input-error" : ""}>
-        <label htmlFor="email">Email: </label>
-        <input
-          id="email"
-          type="text"
-          name="email"
-          onChange={handleChange}
-          value={user.email}
-        />
-      </div>
-      <div className={errors.includes("password") ? "input-error" : ""}>
-        <label htmlFor="password">Password:</label>
-        <input
-          id="password"
-          type="password"
-          name="password"
-          onChange={handleChange}
-          value={user.password}
-        />
-      </div>
-      <div className={errors.includes("confirmation") ? "input-error" : ""}>
-        <label htmlFor="password">Password (Repeat):</label>
-        <input
-          id="confirmation"
-          type="password"
-          name="password"
-          onChange={handleChange}
-          value={user.confirmation}
-        />
-      </div>
-      <button>Next</button>
     </form>
   );
 }
