f69f5f46a19acc510fa18ff2c1324991022fd4db
Sebastian Mottschall <srophkor@gmail.com>
2023-03-12 00:00:24 +0100

    add Language Filter for Lobbies

--- src/pages/Lobbies/Lobbies.jsx
+++ src/pages/Lobbies/Lobbies.jsx
@@ -3,9 +3,12 @@ import api from "../../service/service";
 
 import GameCardList from "../../components/GameCardList/GameCardList";
 import "./Lobbies.css";
+import { Link } from "react-router-dom";
+import Input from "../../components/Input/Input";
 
 function Lobbies() {
   const [rooms, setRooms] = useState([]);
+  const [filter, setFilter] = useState("");
 
   useEffect(() => {
     api
@@ -20,13 +23,33 @@ function Lobbies() {
       .catch((error) => console.log(error));
   }, []);
 
+  function handleFilter(event) {
+    setFilter(event.target.value);
+  }
+
+  const filteredRooms = filter
+    ? rooms.filter((room) =>
+        room.spokenLanguage.toLowerCase().includes(filter.toLowerCase())
+      )
+    : rooms;
+
   return (
     <section className="Lobbies">
       {rooms.length ? (
-        <GameCardList list={rooms} displayLink={true} />
+        <>
+          <Input
+            type="text"
+            name="search"
+            label="Spoken language"
+            action={handleFilter}
+            value={filter}
+          />
+          <GameCardList list={filteredRooms} displayLink={true} />
+        </>
       ) : (
         <h2>
-          There are currently no active lobbies. Would you like to create one?
+          There are currently no active lobbies matching your current filter.
+          <Link to="/lobbies/create">Would you like to create one?</Link>
         </h2>
       )}
     </section>
