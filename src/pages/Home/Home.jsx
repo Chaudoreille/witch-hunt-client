81b0ccad7ff22eda77b2e8118c9c78903bb1e6d1
Sebastian Mottschall <srophkor@gmail.com>
2023-03-11 18:40:26 +0100

    add Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,24 @@
 import React from "react";
+import ButtonLink from "../../components/Button/ButtonLink";
+
 import "./Home.css";
 
 function Home() {
-
   return (
-    <>
-     <h1>Home</h1>
-    </>
+    <section className="Home">
+      <div className="column">
+        {" "}
+        <ButtonLink variant={"primary"} link={"/lobbies/create"}>
+          CREATE A GAME
+        </ButtonLink>
+        <ButtonLink variant={"secondary"} link={"/lobbies"}>
+          FIND A GAME
+        </ButtonLink>
+        <ButtonLink variant={"secondary"} link={"/lobbies/private"}>
+          JOIN A GAME PRIVATE GAME
+        </ButtonLink>
+      </div>
+    </section>
   );
 }
 
