53c1f3092b150afcebd141889c130f4be89098a4
Sebastian Mottschall <srophkor@gmail.com>
2023-03-11 18:17:36 +0100

    add api methods to handle game-room routes

--- src/service/service.js
+++ src/service/service.js
@@ -51,9 +51,122 @@ api.user = async function () {
 
 // Game Room Routes
 
-api.getRooms = async function () {
-  const response = await api.get('/api/game-rooms')
-  return response.data;
+/**
+ * Retreives a list of all gamerooms, or a single room if a pin is specified in the query
+ * 
+ * @returns Array of GameRooms || single GameRoom, if query included a pin
+ */
+api.getRooms = async function (query) {
+  let url = '/api/game-rooms';
+  if (query) {
+    let count = 0;
+    url += '?'
+    for (let key in query) {
+      if (count > 0) url += '&'
+      url += `${key}=${query[key]}`;
+      count++;
+    }
+  }
+  try {
+    const response = await api.get(url)
+    return response.data;
+  } catch (error) {
+    if (error.response) throw Error(error.response.data.message);
+    throw Error('Error when trying to retreive gameroom data:', error.message);
+  }
+}
+
+/**
+ * Creates a new game room on the server. Returns the created GameRoom
+ * @param {GameRoom} room 
+ * @returns 
+ */
+api.createRoom = async function (room) {
+  try {
+    const response = await api.post('/api/game-rooms', room)
+    return response.data;
+  } catch (error) {
+    if (error.response) throw Error(error.response.data.message);
+    throw Error('Error when trying to retreive gameroom data:', error.message);
+  }
+}
+
+/**
+ * Retreives information about the specified room
+ * @param {ObjectId} roomId 
+ * @returns GameRoom
+ */
+api.getRoom = async function (roomId) {
+  try {
+    const response = await api.get(`/api/game-rooms/${roomId}`)
+    return response.data;  
+  } catch (error) {
+    if (error.response) throw Error(error.response.data.message);
+    throw Error('Error when trying to retreive gameroom data:', error.message);
+  }
+}
+
+/**
+ * Updates a GameRoom, returns updated room
+ * @param {ObjectId} roomId 
+ * @param {GameRoom} room 
+ * @returns GameRoom
+ */
+api.updateRoom = async function (roomId, room) {
+  try {
+    const response = await api.patch(`/api/game-rooms/${roomId}`, room)
+    return response.data;
+  } catch (error) {
+    if (error.response) throw Error(error.response.data.message);
+    throw Error('Error when trying to retreive gameroom data:', error.message);
+  }
+}
+
+/**
+ * Deletes specified GameRoom
+ * @param {ObjectId} roomId 
+ * @returns true || false
+ */
+api.deleteRoom = async function (roomId) {
+  try {
+    const response = await api.delete(`/api/game-rooms/${roomId}`)
+    if (response.status === 204) return true;
+    return false;
+  } catch (error) {
+    if (error.response) throw Error(error.response.data.message);
+    throw Error('Error when trying to retreive gameroom data:', error.message);
+  }}
+
+/**
+ * Retreive game state of a specified game room
+ * @param {ObjectId} roomId 
+ * @returns Promise that resolves into a GameState
+ */
+api.getGameState = async function (roomId) {
+  try {
+    const response = await api.patch(`/api/game-rooms/${roomId}/game-state`);
+    return response.data;
+  } catch (error) {
+    if (error.response) throw Error(error.response.data.message);
+    throw Error('Error when trying to retreive game state:', error.message);
+  }
+}
+
+/**
+ * Take an action within a game room.
+ * @param {ObjectId} roomId 
+ * @param {String} action 
+ * @param {ArrayOfStrings} parameters (optional)
+ * @returns Promise that resolves into a GameState
+ */
+api.takeAction = async function (roomId, action, parameters) {
+  try {
+    const response = await api.patch(`/api/game-rooms/${roomId}/game-state`, {action, parameters});
+    return response.data;
+  } catch (error) {
+    if (error.response) throw Error(error.response.data.message);
+    throw Error('Error when trying to submit action:', error.message);
+  }
 }
 
 export default api;
