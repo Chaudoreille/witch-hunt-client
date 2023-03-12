e489e5823a90891adb1947625cb4fb9513b534b9
Chaudoreille <a.chaudoreille@gmail.com>
2023-03-12 21:21:09 +0100

    [PROFILE] - error handling done

--- src/pages/Profile/EditProfile.jsx
+++ src/pages/Profile/EditProfile.jsx
@@ -18,12 +18,6 @@ const EditProfile = ({ mode, setMode }) => {
     email: user.email
   });
 
-  const discardChanges = () => {
-    setUserData({ username: user.username, email: user.email });
-    setMode(null);
-    setErrors({});
-  };
-
   const handleChange = (event) => {
     const { id, value } = event.target;
 
@@ -34,39 +28,54 @@ const EditProfile = ({ mode, setMode }) => {
     setUserData(data => ({ ...data, "image": image }));
   };
 
+  const discardChanges = () => {
+    setUserData({ username: user.username, email: user.email });
+    setMode(null);
+    setErrors({});
+  };
+
   const validateData = () => {
+    const emailRegExp = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
     // To upload file, we have to dress up the entered data as FormData
     const form = new FormData();
+    const errors = {};
 
-    form.append("password", userData.password);
+    // clear error state
+    setErrors({});
 
     if (userData.image) {
       form.append("image", userData.image);
     }
-
     if (userData.username) {
       form.append("username", userData.username);
     }
-
+    if (userData.password) {
+      form.append("password", userData.password);
+    }
     if (userData.email && userData.email !== user.email) {
+      if (!emailRegExp.test(userData.email)) {
+        errors.email = { message: "Invalid email address" };
+      }
+      if (!userData.password) {
+        errors.password = { message: "Please enter your password to change your email or password" };
+      }
       form.append("email", userData.email);
     }
-
     if (userData.newPassword) {
-      if (userData.newPassword === userData.repeatPassword) {
-        form.append("newPassword", userData.newPassword);
-      } else {
-        setErrors(err => ({
-          ...err,
-          newPassword: "Passwords do not match",
-          repeatPassword: ""
-        }));
-        return;
+      if (userData.newPassword !== userData.repeatPassword) {
+        errors.repeatPassword = {};
+        errors.newPassword = { message: "Passwords do not match" };
       }
+      if (!userData.password) {
+        errors.password = { message: "Please enter your password to change your email or password" };
+      }
+      form.append("newPassword", userData.newPassword);
     }
 
-    setErrors({});
-
+    if (Object.keys(errors).length) {
+      setErrors(() => errors);
+      return;
+    }
     return form;
   };
 
@@ -80,22 +89,34 @@ const EditProfile = ({ mode, setMode }) => {
       .then(response => {
         setUser(response.data);
         setMode(null);
+        // Testing for mode change
+        setMode(m => {
+          console.log(m);
+          return m;
+        });
       })
       .catch(error => {
         if (error.response.status === 400) {
-          setErrors(err => ({
-            ...err,
-            email: error.response.data.message,
-          }));
+          setErrors(err => ({ ...err, email: { message: error.response.data.message } }));
         }
 
         if (error.response.status === 401) {
-          setErrors(err => ({
-            ...err,
-            password: error.response.data.message,
-          }));
+          setErrors(err => ({ ...err, password: { message: error.response.data.message } }));
         }
+        console.log(errors);
       });
+  };
+
+  const displayErrors = () => {
+    if (!Object.values(errors).length) return;
+
+    return (
+      <Error>
+        {Object.values(errors).map(error => (
+          error.message && <p className="error-message">{error.message}</p>
+        ))}
+      </Error>
+    );
   };
 
   if (mode === "image") {
@@ -113,54 +134,46 @@ const EditProfile = ({ mode, setMode }) => {
         />
       </div>
     );
-  }
-
-  const displayErrors = () => {
-    if (!Object.keys(errors).length) return;
-
+  } else {
     return (
-      <Error>
-        {Object.values(errors).map(message => (
-          <p className="error-message">{message}</p>
-        ))}
-      </Error>
+      <>
+        <h3>User Information</h3>
+        <form className='EditProfile' onSubmit={submitUser}>
+          {displayErrors()}
+          <Input
+            type="text" name="username" label="Username" placeholder="Username"
+            action={handleChange} value={userData.username}
+          />
+
+          <Input
+            type="email" name="email" label="Email" placeholder="Email"
+            action={handleChange} value={userData.email}
+            className={errors.email && "input-error"}
+          />
+
+          <Input type="password" name="password" label="Password" placeholder="*********"
+            action={handleChange} value={userData.password}
+            className={errors.password && "input-error"}
+          />
+
+          <Input type="password" name="newPassword" label="New Password" placeholder="*********"
+            action={handleChange} value={userData.newPassword}
+            className={errors.newPassword && "input-error"}
+          />
+
+          <Input type="password" name="repeatPassword" label="Repeat Password" placeholder="*********"
+            action={handleChange} value={userData.repeatPassword}
+            className={errors.repeatPassword && "input-error"}
+          />
+
+          <div className="btn-container">
+            <Button type="submit" variant="primary">Save</Button>
+            <Button type="button" variant="secondary" action={discardChanges}>Cancel</Button>
+          </div>
+        </form>
+      </>
     );
-  };
-
-  return (
-    <>
-      <h3>User Information</h3>
-      <form className='EditProfile' onSubmit={submitUser}>
-        {displayErrors()}
-        <Input
-          type="text" name="username" label="Username" placeholder="Username"
-          action={handleChange} value={userData.username}
-        />
-
-        <Input
-          type="email" name="email" label="Email" placeholder="Email"
-          action={handleChange} value={userData.email}
-        />
-
-        <Input type="password" name="password" label="Password" placeholder="*********"
-          action={handleChange} value={userData.password}
-        />
-
-        <Input type="password" name="newPassword" label="New Password" placeholder="*********"
-          action={handleChange} value={userData.newPassword}
-        />
-
-        <Input type="password" name="repeatPassword" label="Repeat Password" placeholder="*********"
-          action={handleChange} value={userData.repeatPassword}
-        />
-
-        <div className="btn-container">
-          <Button type="submit" variant="primary">Save</Button>
-          <Button type="button" variant="secondary" action={discardChanges}>Cancel</Button>
-        </div>
-      </form>
-    </>
-  );
+  }
 };
 
 export default EditProfile;
