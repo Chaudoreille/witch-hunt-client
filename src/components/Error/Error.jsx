d24d8ec6d69c2f470e5b7d7d3da2cba34662cbe8
Chaudoreille <a.chaudoreille@gmail.com>
2023-03-12 21:20:25 +0100

    [ERRORS] - add icon and styling to Error component

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import "./Error.css";
+import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
+// alternative error icon
+// import ErrorIcon from '@mui/icons-material/Error';
 
 const Error = ({ children }) => {
   return (
-    <div className='Error error-message'>
-      {children}
+    <div className='Error'>
+      {children.map(child => (
+        child &&
+        <div className='error-wrapper'>
+          <ErrorOutlineIcon className='error-icon' />
+          {child}
+        </div>
+      ))}
     </div>
   );
 };
