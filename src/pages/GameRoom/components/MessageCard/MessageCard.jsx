661b4f5a227f3abd5e8e1d29429f1c519fe80e6e
Delfina Marchetti <delfinamarchetti@hotmail.it>
2023-03-13 14:04:00 +0100

    add time label to messages

--- src/pages/GameRoom/components/MessageCard/MessageCard.jsx
+++ src/pages/GameRoom/components/MessageCard/MessageCard.jsx
@@ -4,12 +4,15 @@ import "./MessageCard.css";
 
 function MessageCard({ message }) {
   const user = useContext(AuthContext);
+  const creationTime = Math.round(
+    (new Date() - new Date(message.createdAt)) / (1000 * 60)
+  );
+  console.log(creationTime)
 
   return (
     <div
-      className={`MessageCard ${
-        user.user._id === message.author._id ? "sent" : "received"
-      }`}
+      className={`MessageCard ${user.user._id === message.author._id ? "sent" : "received"
+        }`}
     >
       <img
         src={message.author.image}
@@ -17,6 +20,7 @@ function MessageCard({ message }) {
         title={message.author.username}
       />
       <p>{message.content}</p>
+      <p>{creationTime} min ago</p>
     </div>
   );
 }
