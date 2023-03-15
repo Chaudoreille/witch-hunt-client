61df6229c8dac18b811cce322c95794caf3b797d
Chaudoreille <a.chaudoreille@gmail.com>
2023-03-15 16:05:46 +0100

    [BOYSCOUT] remove comments

--- src/pages/GameRoom/components/Messenger/Messenger.jsx
+++ src/pages/GameRoom/components/Messenger/Messenger.jsx
@@ -7,44 +7,12 @@ import MessageCard from "../MessageCard/MessageCard";
 
 function Messenger({ room, handleErrors, sendMessage, messages }) {
   const [currentInput, setCurrentInput] = useState("");
-  // const [messagesLastUpdated] = useState({
-  //   at: null,
-  // });
-
-  // async function loadMessages() {
-  //   api
-  //     .getMessages(room._id, messagesLastUpdated.at)
-  //     .then((response) => {
-  //       if (response.length > 0) {
-  //         setMessages((oldMessages) => [...oldMessages, ...response]);
-  //         messagesLastUpdated.at = response.at(-1).updatedAt;
-  //       }
-  //     })
-  //     .catch((error) => {
-  //       let errorMessage;
-  //       if (error.response) errorMessage = error.response.data.message;
-  //       else errorMessage = error.message;
-  //       handleErrors(errorMessage);
-  //     });
-  // }
 
   function handleSubmit(event) {
     event.preventDefault();
     const message = { content: currentInput };
     sendMessage(message);
     setCurrentInput("");
-
-    // api
-    //   .sendMessage(room._id, currentInput)
-    //   .then((response) => {
-    //     setCurrentInput("");
-    //   })
-    //   .catch((error) => {
-    //     let errorMessage;
-    //     if (error.response) errorMessage = error.response.data.message;
-    //     else errorMessage = error.message;
-    //     handleErrors(errorMessage);
-    //   });
   }
 
   return (
