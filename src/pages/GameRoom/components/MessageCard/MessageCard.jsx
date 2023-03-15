6cc9a60f695fb471cc22dcfed364fe6db5642656
Delfina Marchetti <delfinamarchetti@hotmail.it>
2023-03-15 23:09:38 +0100

    [CSS] chat

--- src/pages/GameRoom/components/MessageCard/MessageCard.jsx
+++ src/pages/GameRoom/components/MessageCard/MessageCard.jsx
@@ -2,21 +2,18 @@ import React, { useContext } from "react";
 import { AuthContext } from "../../../..//context/AuthContext";
 import PassedTime from "../../../../components/PassedTime/PassedTime";
 import "./MessageCard.css";
+import ProfilePicture from "../../../../components/ProfilePicture/ProfilePicture"
 
 function MessageCard({ message }) {
   const user = useContext(AuthContext);
 
   return (
     <div
-      className={`MessageCard ${
-        user.user._id === message.author._id ? "sent" : "received"
-      }`}
+      className={`MessageCard ${user.user._id === message.author._id ? "sent" : "received"
+        }`}
     >
-      <img
-        src={message.author.image}
-        alt={message.author.username}
-        title={message.author.username}
-      />
+      <ProfilePicture user={message.author} />
+
       <div className="column">
         <p className="content">{message.content}</p>
         <p className="timestamp">
