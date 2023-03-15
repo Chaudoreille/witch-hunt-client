e594247ddd6079ce6109d406031350abe792f157
Delfina Marchetti <delfinamarchetti@hotmail.it>
2023-03-15 01:22:20 +0100

    Toggle component

--- src/components/Toggle/Toggle.jsx
+++ src/components/Toggle/Toggle.jsx
@@ -1,39 +1,36 @@
 import React from "react";
 import "./Toggle.css";
 
-function Toggle({ obj, action }) {
+function Toggle({ toggle, optionLeft, optionRight, label, onChangeLeft, onChangeRight }) {
 
   return (
 
-    <div className="input-toggle">
-      <label>Visibility</label>
-      <div className="visib-toggle">
+    <div className="input-toggle Toggle">
+      <label>{label}</label>
+      <div className="toggle">
         <input
-          checked={obj.isPublished}
+          checked={toggle}
           type="radio"
-          id="visib-toggle-public"
-          name="visib"
-          value={obj.isPublished}
-          onChange={(event) => {
-            action({ isPublished: event.target.checked });
-          }}
+          id="toggle-left"
+          name="toggle"
+          value={toggle}
+          onChange={onChangeLeft}
         ></input>
-        <label className="radio-button" htmlFor="visib-toggle-public">
-          Public
+        <label className="radio-button" htmlFor="toggle-left">
+          {optionLeft}
         </label>
 
         <input
-          checked={!obj.isPublished}
+          checked={!toggle}
           type="radio"
-          id="visib-toggle-private"
-          name="visib"
-          value={!obj.isPublished}
-          onChange={(event) => {
-            action({ isPublished: !event.target.checked });
-          }}
+          id="toggle-right"
+          name="toggle"
+          value={!toggle}
+          onChange={onChangeRight}
+
         ></input>
-        <label className="radio-button" htmlFor="visib-toggle-private">
-          Private
+        <label className="radio-button" htmlFor="toggle-right">
+          {optionRight}
         </label>
       </div>
     </div>
