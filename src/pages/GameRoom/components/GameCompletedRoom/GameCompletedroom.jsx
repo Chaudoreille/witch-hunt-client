717f73c2c06ea66ce8bac8cf1744a05dff7cefb7
Chaudo <a.chaudoreille@gmail.com>
2023-03-16 20:11:54 +0100

    Game Over management (#20)

--- src/pages/GameRoom/components/GameCompletedRoom/GameCompletedroom.jsx
+++ src/pages/GameRoom/components/GameCompletedRoom/GameCompletedroom.jsx
@@ -1,16 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import api from "../../../../service/service";
 import "./GameCompleted.css";
 import ButtonLink from "../../../../components/Button/ButtonLink";
+import Button from "../../../../components/Button/Button";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../../../context/AuthContext";
+
 /**
  * GameCompletedroom
  * This component will display the end of game result page.
  * @returns JSX.Element
  */
-function GameCompletedroom({ winners }) {
+function GameCompletedroom({ room, winners }) {
   const [show, setShow] = useState(false);
+  const { user } = useContext(AuthContext);
+
+  const navigate = useNavigate();
   setTimeout(() => {
     setShow(true);
   }, 1500);
+
+  function thatsAllFolks() {
+    api.deleteRoom(room._id)
+      .then(() => navigate('/home'))
+      .catch(error => {
+        console.error(error);
+        navigate('/home');
+      });
+  }
 
   return (
     <section className={`GameCompletedRoom ${show ? "show" : ""}`}>
@@ -18,6 +35,9 @@ function GameCompletedroom({ winners }) {
         <img src="/images/witch-run_logo.png" alt="logo" />
         <h2>The {winners} won !</h2>
         <ButtonLink link="/home" className="new-game" variant="small">New game</ButtonLink>
+        {room.owner === user._id && (
+          <Button variant="small" className="new-game" action={thatsAllFolks}>Delete Room</Button>
+        )}
       </div>
     </section>
   );
