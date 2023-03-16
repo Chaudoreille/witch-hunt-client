a3316dc58f091c2f1545ed49811f5f128f0beaf6
Delfina Marchetti <delfinamarchetti@hotmail.it>
2023-03-16 12:05:43 +0100

    sun and moon icons in storytime

--- src/pages/GameRoom/components/Storytime/Storytime.jsx
+++ src/pages/GameRoom/components/Storytime/Storytime.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import "./Storytime.css";
 
-const Storytime = ({ story, time, state }) => {
+const Storytime = ({ story, time, status }) => {
+  console.log(status)
   return (
     <div className='Storytime'>
       <p className='story'>
-        <span>
-          {state === "Active" && (time === "Daytime" &&
-            <img src="/images/sun.png" alt="sun" />
-            || <img src="/images/moon.png" alt="sun" />)
-          }
-        </span>
+        {status === "Started" &&
+          <span>
+            {time === "Daytime" &&
+              <img src="/images/sun.png" alt="sun" />
+              || <img src="/images/moon.png" alt="sun" />
+            }
+          </span>
+        }
 
         {story}
 
