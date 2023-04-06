2afe3ab2f7e60bd7798eea736e1b6e80871970fa
Sebastian Mottschall <srophkor@gmail.com>
2023-04-06 19:43:43 +0200

    [AUDIO] implement SoundManager Functionality - with Sound Effects and Background Music

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,19 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import AuthContextWrapper from "./context/AuthContext";
 import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary";
+import SoundManagerContextWrapper from "./context/SoundManagerContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthContextWrapper>
-        <ErrorBoundary fallback={<p className="error-message">There was an error</p>}>
-          <App />
-        </ErrorBoundary>
+        <SoundManagerContextWrapper>
+          <ErrorBoundary
+            fallback={<p className="error-message">There was an error</p>}
+          >
+            <App />
+          </ErrorBoundary>
+        </SoundManagerContextWrapper>
       </AuthContextWrapper>
     </BrowserRouter>
   </React.StrictMode>
